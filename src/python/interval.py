a22e4b3e16640f7fa24405b935cceff2d20681e7
David P. Sanders <dpsanders@ciencias.unam.mx>
2014-03-19 17:02:11 -0600

    Changed repr of interval to give correct representation as an object construction

--- src/python/interval.py
+++ src/python/interval.py
@@ -80,7 +80,7 @@ class Interval(object):
     # Formatting functions:
     #
     def __repr__(self):
-        return "Interval[{}, {}]".format(self.lo, self.hi)
+        return "Interval({}, {})".format(repr(self.lo), repr(self.hi))
         #return "Interval[{}, {}]".format(repr(self.lo), repr(self.hi))
 
 
@@ -791,74 +791,3 @@ class Interval(object):
 #        raise ValueError("No such rounding mode; setting it to RoundToNearest")
 #
 #    return a
-
-
-def random_interval( infimum=-10.0, supremum=10.0 ):
-    num1a = np.random.uniform( infimum, supremum )
-    num2a = np.random.uniform( infimum, supremum )
-
-    return Interval( num1a, num2a )
-
-def split_interval( x, num_divisions=1 ):
-    """
-    Divide un Interval en n=num_divisions Intervals iguales
-    """
-    num_divisions = int(num_divisions)
-    if num_divisions < 1:
-        num_divisions = 1
-
-    edge_points = np.linspace(x.lo, x.hi, num_divisions+1)
-    splited_intervals = [Interval(a, b) for (a,b) in
-                         zip(edge_points[0:num_divisions+1],edge_points[1:num_divisions+2]) ]
-
-    return splited_intervals
-
-def range_interval_f( fun, subdivided_interval ):
-    """
-    Evalua la función f(x) extendida sobre Intervals, en una lista de subIntervals
-    y regresa el hull de todos ellos, es decir, una cota del rango de la función
-    """
-    if not isinstance( subdivided_interval, list ):
-        subdivided_interval = [ subdivided_interval ]
-
-    range_fun = [ fun(i) for i in subdivided_interval ]
-    range_tot = range_fun[0]
-
-    for i in range_fun[1:]:
-        range_tot = range_tot.hull(i)
-
-    return range_tot
-
-def plot_interval_f( fun, x, pow2=0, num_points=101 ):
-    """
-    This plots the interval extension of a function `fun` over the interval `x`,
-    which is diveded in num=1,2,4,...,2**pow2 uniform subintervals.
-    """
-    num_intervals = [ 2**p for p in range(pow2+1) ]
-    plt.figure()
-    plt.subplot(1, 1, 1)
-
-    for num in num_intervals:
-        fact_alfa = num*1.0/num_intervals[-1]   # for plotting
-
-        # Se divide los subIntervale en 2**num subIntervals iguales
-        subdivided_intervals = split_interval( x, num )
-        # Se calculan las extensiones de la función sobre el Interval, usando los subIntervals
-        rango_total = range_interval_f( fun, subdivided_intervals )
-        print "Rango_tot (N={}) = {}".format(num,rango_total)
-
-        # Hago el dibujo
-        for x1 in subdivided_intervals:
-            low = float(x1.lo)
-            high = float(x1.hi)
-            Ffun = fun(x1)
-            xa1 = np.array([low, low, high, high])
-            ya1 = np.array([float(Ffun.lo), float(Ffun.hi), float(Ffun.hi), float(Ffun.lo) ])
-            plt.fill( xa1, ya1, 'b', alpha=fact_alfa )
-
-    low = float(x.lo)
-    high = float(x.hi)
-    xx = np.linspace(low,high,num_points)
-    yy = fun(xx)
-    plt.plot( xx, yy, 'red')
-    return
